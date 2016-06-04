3c641bcb69f42d2459c4733ca1bc4b6634b1a267
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2016-06-04 13:47:21 -0500

    added withpost build type

---
+++ WithPost/src/NonLinear.d
@@ -0,0 +1,40 @@
+src/NonLinear.d: ../src/NonLinear.cpp ../include/NonLinear.h \
+ ../../ClusteredCore/include/forceManager.h \
+ ../../ClusteredCore/include/config.h ../../ClusteredCore/include/defs.h \
+ ../../ClusteredCore/include/diagnostics.h \
+ ../../ClusteredCore/include/structs/type3.h \
+ ../../ClusteredCore/include/particle.h \
+ ../../ClusteredCore/include/utilities.h \
+ ../../ClusteredCore/include/timer.h ../../ClusteredCore/include/error.h \
+ ../../ClusteredCore/include/structs/systemState.h \
+ ../../ClusteredCore/include/structs/type3.h \
+ ../../ClusteredCore/include/interfaces/IForce.h \
+ ../../ClusteredCore/include/utilities.h
+
+../include/NonLinear.h:
+
+../../ClusteredCore/include/forceManager.h:
+
+../../ClusteredCore/include/config.h:
+
+../../ClusteredCore/include/defs.h:
+
+../../ClusteredCore/include/diagnostics.h:
+
+../../ClusteredCore/include/structs/type3.h:
+
+../../ClusteredCore/include/particle.h:
+
+../../ClusteredCore/include/utilities.h:
+
+../../ClusteredCore/include/timer.h:
+
+../../ClusteredCore/include/error.h:
+
+../../ClusteredCore/include/structs/systemState.h:
+
+../../ClusteredCore/include/structs/type3.h:
+
+../../ClusteredCore/include/interfaces/IForce.h:
+
+../../ClusteredCore/include/utilities.h:
